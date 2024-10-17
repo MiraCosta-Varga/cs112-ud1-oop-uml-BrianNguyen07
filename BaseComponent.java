bc71ab19e250261ffd98762982fdedb47bcec46e
BrianNguyen07 <briannnguyen2007@gmail.com>
2024-10-17 22:22:04 +0000

    project

---
+++ BaseComponent.java
@@ -0,0 +1,4 @@
+public abstract class BaseComponent {
+    // Abstract method to be implemented by subclasses
+    public abstract void render();
+}
---
+++ InvalidUserInputException.java
@@ -0,0 +1,6 @@
+public class InvalidUserInputException extends Exception {
+    // Constructor to pass an error message
+    public InvalidUserInputException(String message) {
+        super(message);
+    }
+}
